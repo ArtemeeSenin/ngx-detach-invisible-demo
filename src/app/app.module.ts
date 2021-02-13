d80578bf604ea3700eec03b2f36d28d594bfd55b
ArtemeeSenin <artemee.senin@gmail.com>
2021-02-13 04:31:37 +0300

    Update ngx-detach-invisible and add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { DetachInvisibleModule, DetachInvisibleService} from 'ngx-detach-invisible';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { EmojiCounterComponent } from './emoji-counter/emoji-counter.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, EmojiCounterComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, DetachInvisibleModule ],
+  declarations: [ AppComponent, EmojiCounterComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ DetachInvisibleService ]
 })
 export class AppModule { }
